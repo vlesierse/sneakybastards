0b0d8ebc14958f37634e249ef8bace3abc7c4052
Vincent Lesierse <vincent@lesierse.com>
2016-08-30 13:38:50 +0200

    Initial commit

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(LineRenderer))]
+public class EnemyAI : MonoBehaviour
+{
+
+	public Transform player;
+
+	public float fieldOfViewAngle = 120;
+	public float fieldOfViewDistance = 5;
+
+	public bool followPlayer;
+
+	private LineRenderer _lineRenderer;
+
+	void Start()
+	{
+		_lineRenderer = GetComponent<LineRenderer>();
+		_lineRenderer.SetVertexCount(5);
+
+		iTween.RotateBy(gameObject, iTween.Hash(
+			"delay", 1,
+			"y", 1,
+			"time", 5,
+			"easetype", iTween.EaseType.linear,
+			"looptype", iTween.LoopType.pingPong));
+	}
+
+	void Update()
+	{
+		if (!FollowPlayer())
+		{
+			var lookDirection = player.position - transform.position;
+			if ((Vector3.Angle(lookDirection, transform.forward)) <= (fieldOfViewAngle / 2))
+			{
+				if (Physics.Raycast(transform.position, lookDirection, fieldOfViewDistance / 2))
+				{
+					followPlayer = true;
+					iTween.Stop(gameObject);
+				}
+			}
+		}
+		UpdateFieldOfView();
+	}
+
+	bool FollowPlayer()
+	{
+		if (followPlayer)
+		{
+			transform.LookAt(player);
+		}
+		return followPlayer;
+	}
+
+	void UpdateFieldOfView()
+	{
+		var localLookHorizon = Vector3.forward * fieldOfViewDistance;
+		// Render the gizmo
+		_lineRenderer.SetPosition(0, Vector3.zero);
+		_lineRenderer.SetPosition(1, Quaternion.AngleAxis(fieldOfViewAngle / 2, Vector3.up) * localLookHorizon);
+		_lineRenderer.SetPosition(2, localLookHorizon);
+		_lineRenderer.SetPosition(3, Quaternion.AngleAxis(-(fieldOfViewAngle / 2), Vector3.up) * localLookHorizon);
+		_lineRenderer.SetPosition(4, Vector3.zero);
+	}
+
+}
---
+++ Assets/Scripts/MoveByMouseClick.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveByMouseClick : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast(ray, out hit))
+			{
+				var destination = new Vector3(hit.point.x, transform.position.y, hit.point.z);
+				iTween.MoveTo(gameObject, destination, 2);
+				// Calculate the direction of the target and rotation
+				var lookDirection = (destination - transform.position).normalized;
+				var lookRotation = Quaternion.LookRotation(lookDirection);
+				iTween.RotateTo(gameObject, lookRotation.eulerAngles, 1);
+			}
+		}
+	}
+
+}
